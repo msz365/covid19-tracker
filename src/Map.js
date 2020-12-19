973a49a8c0a0899aa6e7ab5776254f6c65f806a2
msz365 <31920881+msz365@users.noreply.github.com>
2020-12-20 01:51:46 +0500

    commit

---
+++ src/Map.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import './Map.css'
+import { Map as LeafletMap, TileLayer, Marker, Popup } from 'react-leaflet'
+import "leaflet/dist/leaflet.css"
+import { showDataOnMap } from './util'
+
+function Map({countries, casesType, center, zoom, scrollWheelZoom}) {
+
+    return (
+        <div className="map">
+           
+
+
+            <LeafletMap center={center} zoom={zoom} scrollWheelZoom={scrollWheelZoom}>
+                <TileLayer
+                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                {showDataOnMap(countries, casesType)}
+            </LeafletMap>
+        </div>
+
+    
+    )
+}
+
+export default Map;
